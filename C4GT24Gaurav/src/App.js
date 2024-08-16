e419c4d8db07b63ced114006e1e8afdd64b7ddfb
Gaurav05082002 <gaurav05082002@gmail.com>
2024-08-16 22:22:02 +0530

    code cleaning

--- C4GT24Gaurav/src/App.js
+++ C4GT24Gaurav/src/App.js
@@ -4,11 +4,11 @@ import CreatorLoginPage from './pages/Auth/CreatorLoginPage';
 import CreatorSignupPage from './pages/Auth/CreatorSignupPage';
 import OrgLoginPage from './pages/Auth/OrgLoginPage';
 import SelectedUserLoginPage from './pages/Auth/SelectedUserLoginPage';
-import NoAccessPage from './pages/Auth/NoAccessPage';
+
 import Home from './pages/Home/Home';
 import Header from './components/Common/Header';
 import Footer from './components/Common/Footer';
-import SelecetdUserSignsupPage from './pages/Auth/SelectedUserSignsupPage'
+
 import ActivatePage from './pages/Auth/ActivatePage';
 import ResetPasswordPage from './pages/Auth/ResetPasswordPage';
 import UserHomePage from './pages/Landing/UserHomePage';
@@ -65,8 +65,8 @@ function App() {
           <Route path="/creator-signup" element={<CreatorSignupPage/>} />
           {/* <Route path="/org-login" element={<OrgLoginPage/>} /> */}
           <Route path="/selected-user-login" element={<SelectedUserLoginPage/>} />
-          <Route path="/no-access" element={<NoAccessPage/>} />
-          <Route path="/selected-user-signup" exact element={<SelecetdUserSignsupPage/>} />
+         
+
           <Route path="/auth/activate" element={<ActivatePage/>} />
           <Route path="/auth/reset-password" element={<ResetPasswordPage/>} />
           {/* <Route path="/user-home" element={<UserHomePage/>} /> */}
